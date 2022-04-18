2557c4c03d4a3731dbd0dd503d18d0dbc7d4db10
vandylins19 <shuyang.lin@vanderbilt.edu>
2022-04-18 11:45:39 -0500

    prototype uploaded

---
+++ README.md
@@ -0,0 +1,16 @@
+# email-assistant-openai
+A project for DS5899 Transformer
+
+2022/03/16
+Demo created
+
+# How to run the demo:
+
+* Sign up for an OpenAI account
+* Clone this repo to local
+* Insert your private key in backend.py
+* Install streamlit and openai by running commands:
+  * pip install streamlit
+  * pip install openai
+* Run the frontend locally by running the command in the project path:
+  * streamlit run frontend.py
---
+++ backend.py
@@ -0,0 +1,22 @@
+import openai
+
+class EmailAssistant:
+    def __init__(self, key):
+        self.key = key
+
+    #     
+    def email(self, start:str = "Hi, John Doe", len:int = 250, draft:str = "recruiter, give me an referral for internship.", topic:str = 'Request for referral', random_state:float = 0.9 ) -> str:
+        openai.api_key = self.key
+        return openai.Completion.create(
+            engine='text-davinci-002',
+            prompt="Write me a professional style email start with '" + start + "' about '" + topic + "' and the email should be based on this draft: \"" + draft + "\"\n",
+            temperature = random_state,
+            max_tokens= len,
+            top_p=1,
+            frequency_penalty=1,
+            presence_penalty=1,
+            best_of=2
+        ).get("choices")[0]['text'].strip()
+
+
+
